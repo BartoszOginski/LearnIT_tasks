c9725c10788f978b87aed64d2ae4656d5de89f69
Bartosz Ogiński <bartolino1@op.pl>
2026-08-14 17:39:30 +0200

    drobne zmiany

--- lekcja_12/Task6_Walidacja Hasła.py
+++ lekcja_12/Task6_Walidacja Hasła.py
@@ -1,15 +1,2 @@
-class InvalidPasswordError(Exception):
+class InvalidPasswordError (Exception):
     pass
-
-
-def ustaw_haslo(haslo):
-    if len(haslo) < 8:
-        raise InvalidPasswordError("Hasło musi mieć co najmniej 8 znaków.")
-    
-    print("Hasło zostało ustawione.")
-
-
-try:
-    ustaw_haslo("abc")
-except InvalidPasswordError as blad:
-    print("Błąd:", blad)

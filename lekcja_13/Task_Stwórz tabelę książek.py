5069ad34e11f172f6bacffe9f155be3f926d0950
Bartosz Ogiński <bartolino1@op.pl>
2026-08-16 20:34:01 +0200

    Zadania - lekcja 13

---
+++ lekcja_13/Task_Stwórz tabelę książek.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('biblioteka.db')
+c = conn.cursor()
+
+c.execute('''
+CREATE TABLE IF NOT EXISTS ksiazki(
+id INTEGER PRIMARY KEY,
+tytul TEXT NOT NULL,
+autor TEXT NOT NULL,
+rok_wydania INTEGER
+)
+''')
+
+conn.commit()
+
+print("Tabela 'ksiazki' została utworzona")
+
+conn.close()
